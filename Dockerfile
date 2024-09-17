c3f8f4d8724ff461937e9dfd5ca0b107bc1e43aa
Seepana NavyaSree <navyasree.seepana@publicissapient.com>
2024-09-17 16:15:46 +0530

    changes

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /capsuled
 COPY package*.json ./
 RUN npm install 
 COPY . .
-EXPOSE 3000
+EXPOSE 3002
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // Middleware
 app.use(cors());
